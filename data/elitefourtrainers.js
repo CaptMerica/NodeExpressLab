a57f17ec786dab29233de189b3da219f2c6c2eb2
CaptMerica <rmerica@live.com>
2023-01-04 20:48:40 -0500

    add views

---
+++ data/elitefourtrainers.js
@@ -0,0 +1,11 @@
+const eliteFourTrainers = [
+  {name: 'Lorelei', typeSpecialty: 'Ice', pokemon: 5},
+  {name: 'Bruno', typeSpecialty: 'Fighting', pokemon: 5},
+  {name: 'Agatha', typeSpecialty: 'Ghost', pokemon: 5},
+  {name: 'Lance', typeSpecialty: 'Dragon', pokemon: 5},
+  {name: 'Blue', typeSpecialty: 'Various', pokemon: 6}
+]
+
+export { 
+	eliteFourTrainers
+}
